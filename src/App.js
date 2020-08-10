ef5ce09df385e019fe3fc2965b4cb62b2fdfc082
hiranumaharuka <colorcord251@gmail.com>
2020-08-10 22:11:16 +0900

    header作成途中

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
 import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <HelloWorld name="Jarot" />
       {/* <CounterExample /> */}
     </div>
