c7db93458dff3f33fd3bdb11e1ce265a8b883c75
hiranumaharuka <colorcord251@gmail.com>
2020-08-19 22:41:28 +0900

    routing追加

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useTransition, animated } from 'react-spring';
+import NavigationMenu from './NavigationMenu';
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
   const maskTransitions = useTransition(showMenu, null, {
@@ -39,10 +40,7 @@ function Navigation() {
               style={props}
               className="fixed bg-white top-0 left-0 w-4/5 z-50 h-full shadow p-3"
             >
-              <span className="font-bold">The menu</span>
-              <ul>
-                <li>Home</li>
-              </ul>
+              <NavigationMenu closeMenu={() => setShowMenu(false)} />
             </animated.div>
           )
       )}
